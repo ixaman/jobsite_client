247f81cba0b477855e51e3b86092a86d1856e65b
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-03 22:16:50 +0600

    part 3 added

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import JobCard from "../components/reusable/JobCard";
 
 const Jobs = () => {
-  const navigate = useNavigate();
-
   return (
     <div className='pt-14'>
-      <h1>This is job page</h1>
-      <div>
-        <h1>THIS IS A JOB</h1>
-        <button className='border' onClick={() => navigate("/job-details")}>
-          Details
-        </button>
+      <div className='bg-primary/10 p-5 rounded-2xl'>
+        <h1 className='font-semibold text-xl'>Find Jobs</h1>
+      </div>
+      <div className='grid grid-cols-2 gap-5 mt-5'>
+        <JobCard />
       </div>
     </div>
   );
